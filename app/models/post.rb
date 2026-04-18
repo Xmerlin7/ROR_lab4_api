1267c7dd9b1394945a2fa43a8e4b68e2ac940674
Xmerlin7 <seifahmedjr7@gmail.com>
2026-04-18 15:34:21 +0200

    Implement user and post models with corresponding controllers, routes, and migrations; add tests and fixtures for both entities

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,2 @@
 class Post < ApplicationRecord
-  belongs_to :creator, class_name: "User", foreign_key: :creator_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_18_132256) do
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.integer "creator_id"
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
 end
